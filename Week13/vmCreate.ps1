aead2d6473dfb704ca499d148a105cbe2e675921
maziar <sojodyan@gmail.com>
2024-11-26 14:00:39 -0500

    add myVnet

--- Week13/vmCreate.ps1
+++ Week13/vmCreate.ps1
@@ -1,4 +1,8 @@
-$resourceGroupName = "MyResourceGroup"
-$location = "East US"
+$resourceGroupName = 'MyResourceGroup'
+$location = 'East US'
 
+# Create a new resource group
 New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a new virtual network
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Location $location -Name 'MyVnet' -AddressPrefix '10.0.0.0/16'
